fbdf0dd1c47bff0b80b0d1a77b346229115ae955
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-11-19 18:36:02 -0500

    First rendered output
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ blackjack_lua/run/svg_path.lua
@@ -0,0 +1,167 @@
+local P = require("params")
+local NodeLibrary = require("node_library")
+local V = require("vector_math")
+
+local parse_parameters = function(d)
+    local params = {}
+    while string.len(d) > 0 do
+        local _, j, m = string.find(d, "^%s*([%d%.%-]+)%s*")
+        if m ~= nil then
+            table.insert(params, 0+m)  -- coerce m to a number.
+            d = string.sub(d, j+1)
+            continue
+        end
+
+        -- Should not reach here.
+        print("programming error - parse_parameters")
+        return params
+    end
+    return params
+end
+
+local parse_path = function(d)
+    local path_steps = {}
+    while string.len(d) > 0 do
+        local i, j = string.find(d, "^%s+")
+        if i ~= nil then
+            d = string.sub(d, j+1)
+        end
+
+        local _, j, m = string.find(d, "^(z)%s*")
+        if m ~= nil then
+            table.insert(path_steps, {C="z"})
+            d = string.sub(d, j+1)
+            continue
+        end
+
+        local _, j, m, n = string.find(d, "^([mlhvcsqtaMLHVCSQTA])([%d%.%-,%s]*)")
+        if m ~= nil then
+            local params = parse_parameters(n)
+            table.insert(path_steps, {C=m, P=params})
+            d = string.sub(d, j+1)
+            continue
+        end
+
+        -- Should not reach here, as this is an unsupported SVG command.
+        return path_steps
+    end
+    return path_steps
+end
+
+local cmd_close_path = function(state)
+    if #state.points > 0 then
+        if state.mesh ~= nil then
+            Ops.merge(state.mesh, Primitives.polygon(state.points))
+        else
+            state.mesh = Primitives.polygon(state.points)
+        end
+        state.points = {}
+    end
+end
+
+local cmd_move_to_abs = function(state, params)
+    cmd_close_path(state)
+    state.current_pos = state.pos + params[1] * state.right + params[2] * state.normal
+    table.insert(state.points, state.current_pos)
+    return state
+end
+
+local cmd_move_to = function(state, params)
+    cmd_close_path(state)
+    state.current_pos = state.current_pos + params[1] * state.right + params[2] * state.normal
+    table.insert(state.points, state.current_pos)
+    return state
+end
+
+local cmd_line_to_abs = function(state, params)
+    state.current_pos = state.pos + params[1] * state.right + params[2] * state.normal
+    table.insert(state.points, state.current_pos)
+    return state
+end
+
+local cmd_line_to = function(state, params)
+    state.current_pos = state.current_pos + params[1] * state.right + params[2] * state.normal
+    table.insert(state.points, state.current_pos)
+    return state
+end
+
+local all_commands = {
+    M = cmd_move_to_abs,
+    m = cmd_move_to,
+    L = cmd_line_to_abs,
+    l = cmd_line_to,
+    Z = cmd_close_path,
+    z = cmd_close_path,
+}
+
+-- A path_step represents a single path step.
+--
+-- There are 20 possible commands, broken up into 6 types,
+-- with each command having an "absolute" (upper case) and
+-- a "relative" (lower case) version.
+--
+-- MoveTo: M, m
+-- LineTo: L, l, H, h, V, v
+-- Cubic Bézier Curve: C, c, S, s
+-- Quadratic Bézier Curve: Q, q, T, t
+-- Elliptical Arc Curve: A, a
+-- ClosePath: Z, z
+--
+-- The 'C' field is the command, and the 'P' field is the numeric parameters.
+local process_path_step = function(state, path_step)
+    local cmd = all_commands[path_step.C]
+    if cmd == nil then
+        return state  -- error - command not found
+    end
+    state = cmd(state, path_step.P)
+    return state
+end
+
+NodeLibrary:addNodes(
+    {
+        SVGPath = {
+            label = "SVG Path",
+            op = function(inputs)
+                local normal = V.normalize(inputs.normal)
+                local right = V.normalize(inputs.right)
+                local state = {
+                    normal = normal,
+                    pos = inputs.pos,
+                    current_pos = inputs.pos,
+                    right = right,
+                    segments = inputs.segments,
+                    size = inputs.size,
+                    points = {},
+                    mesh = nil,
+                }
+
+                if inputs.segments < 1 then
+                    return {
+                        out_mesh = state.mesh
+                    }
+                end
+
+                local path_steps = parse_path(inputs.path)
+                for _, path_step in path_steps do
+                    process_path_step(state, path_step)
+                end
+
+                cmd_close_path(state)
+
+                return {
+                    out_mesh = state.mesh
+                }
+            end,
+            inputs = {
+                P.lua_str("path"),
+                P.v3("pos", vector(0, 0, 0)),
+                P.v3("normal", vector(0, 1, 0)),
+                P.v3("right", vector(1, 0, 0)),
+                P.v3("size", vector(1, 1, 1)),
+                P.scalar_int("segments", {default = 36, min = 0, soft_max = 360}),
+            },
+            outputs = {P.mesh("out_mesh")},
+            returns = "out_mesh"
+        }
+    }
+)
