8b7fc07b28b43f537eba354d4708d03f099d412c
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-11-20 19:13:45 -0500

    Remove debugging
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- blackjack_lua/run/svg_path.lua
+++ blackjack_lua/run/svg_path.lua
@@ -84,7 +84,6 @@ end
 
 local cmd_move_to_abs = function(state, params)
     terminate_path(state)
-    -- print("pos=",state.pos,", params[1]=",params[1],", right=", state.right,", params[2]=",params[2],", normal=",state.normal)
     state.current_pos = state.pos + params[1] * state.right + params[2] * state.normal
     table.insert(state.points, state.current_pos)
     return state
@@ -236,11 +235,8 @@ end
 local cmd_quadratic_bezier_curve_abs = function(state, params)
     for i = 1, #params, 4 do
         local p0 = state.current_pos
-        -- print("cmd_quadratic_bezier_curve_abs: p0=",p0)
         local p1 = state.pos + params[i  ] * state.right + params[i+1] * state.normal
-        -- print("cmd_quadratic_bezier_curve_abs: p1=",p1)
         local p2 = state.pos + params[i+2] * state.right + params[i+3] * state.normal
-        -- print("cmd_quadratic_bezier_curve_abs: p2=",p2)
         state = quadratic_to(state, p0, p1, p2)
         state.last_p1 = p1
         state.last_p2 = p2
@@ -251,11 +247,8 @@ end
 local cmd_quadratic_bezier_curve = function(state, params)
     for i = 1, #params, 4 do
         local p0 = state.current_pos
-        -- print("cmd_quadratic_bezier_curve: p0=",p0)
         local p1 = state.current_pos + params[i  ] * state.right + params[i+1] * state.normal
-        -- print("cmd_quadratic_bezier_curve: p1=",p1)
         local p2 = state.current_pos + params[i+2] * state.right + params[i+3] * state.normal
-        -- print("cmd_quadratic_bezier_curve: p2=",p2)
         state = quadratic_to(state, p0, p1, p2)
         state.last_p1 = p1
         state.last_p2 = p2
@@ -266,19 +259,11 @@ end
 local cmd_smooth_quadratic_bezier_curve_abs = function(state, params)
     for i = 1, #params, 2 do
         local p0 = state.current_pos
-        -- print("cmd_smooth_quadratic_bezier_curve_abs: p0=",p0)
         local p1 = state.current_pos
-        -- print("cmd_smooth_quadratic_bezier_curve_abs: p1=",p1)
-        -- print("cmd_smooth_quadratic_bezier_curve_abs: state.last_cmd=",state.last_cmd)
         if state.last_cmd == "Q" or state.last_cmd == "q" or state.last_cmd == "T" or state.last_cmd == "t" then
-            -- print("cmd_smooth_quadratic_bezier_curve_abs: state.current_pos=",state.current_pos)
-            -- print("cmd_smooth_quadratic_bezier_curve_abs: state.last_p2=",state.last_p2)
-            -- print("cmd_smooth_quadratic_bezier_curve_abs: state.last_p1=",state.last_p1)
             p1 = state.current_pos + state.last_p2 - state.last_p1
-            -- print("cmd_smooth_quadratic_bezier_curve_abs: B. p1=",p1)
         end
         local p2 = state.pos + params[i  ] * state.right + params[i+1] * state.normal
-        -- print("cmd_smooth_quadratic_bezier_curve_abs: p2=",p2)
         state = quadratic_to(state, p0, p1, p2)
         state.last_p1 = p1
         state.last_p2 = p2
@@ -303,13 +288,9 @@ end
 
 local angle_between = function(v0, v1)
     local p =  v0.x*v1.x + v0.y*v1.y
-    -- print("p=",p)
     local n = math.sqrt((math.pow(v0.x, 2)+math.pow(v0.y, 2)) * (math.pow(v1.x, 2)+math.pow(v1.y, 2)))
-    -- print("n=",n)
     local sign = v0.x*v1.y - v0.y*v1.x < 0 and -1 or 1
-    -- print("sign=",sign)
     local angle = sign*math.acos(p/n)
-    -- print("angle=",angle)
     return angle
 end
 
@@ -320,94 +301,54 @@ local elliptic_arc_to = function(state, rx, ry, angle, large_arc_flag, sweep_fla
         table.insert(state.points, state.current_pos)
         return
     end
-    print("elliptic_arc_to: current_pos=",state.current_pos)
-    print("rx=",rx)
-    print("ry=",ry)
-    print("angle=",angle,"in degrees:", angle*180/math.pi)
-    print("large_arc_flag=",large_arc_flag)
-    print("sweep_flag=",sweep_flag)
-    print("p0=",p0)
-    print("p1=",p1)
 
     local d = 0.5*(p0 - p1)
-    print("d=",d)
     local dx = V.dot(d, state.right)
-    print("dx=",dx)
     local dy = V.dot(d, state.normal)
-    print("dy=",dy)
     local tpx = math.cos(angle)*dx + math.sin(angle)*dy
-    print("tpx=",tpx)
     local tpy = -math.sin(angle)*dx + math.cos(angle)*dy
-    print("tpy=",tpy)
     local radii_check = math.pow(tpx,2)/math.pow(rx,2) + math.pow(tpy,2)/math.pow(ry,2)
-    print("radii_check=",radii_check)
     if radii_check > 1 then
         rx = math.sqrt(radii_check)*rx
         ry = math.sqrt(radii_check)*ry
     end
-    print("B. rx=",rx)
-    print("B. ry=",ry)
 
     local sn = math.pow(rx,2)*math.pow(ry,2) - math.pow(rx,2)*math.pow(tpy,2) - math.pow(ry,2)*math.pow(tpx,2)
-    print("sn=",sn)
     local srd = math.pow(rx,2)*math.pow(tpy,2) + math.pow(ry,2)*math.pow(tpx,2)
-    print("srd=",srd)
     local radicand = sn/srd
-    print("radicand=",radicand)
     if radicand < 0 then
         radicand = 0
     end
-    print("B. radicand=",radicand)
 
     local coef = large_arc_flag ~= sweep_flag and math.sqrt(radicand) or -math.sqrt(radicand)
-    print("coef=",coef)
     local tcx = coef*((rx*tpy)/ry)
-    print("tcx=",tcx)
     local tcy = coef*(-(ry*tpx)/rx)
-    print("tcy=",tcy)
     local tc = tcx * state.right + tcy * state.normal
-    print("tc=",tc)
 
     local center = rotate_around_axis(angle, tcx, tcy, state.right, state.normal) + (0.5*(p0 + p1))
-    print("center=",center)
 
     local start_vector = vector((tpx - tcx)/rx, (tpy - tcy)/ry, 0)
-    print("start_vector=",start_vector)
     local start_angle = angle_between(vector(1,0,0), start_vector)
-    print("start_angle=",start_angle,"in degrees:", start_angle*180/math.pi)
 
     local end_vector = vector((-tpx - tcx)/rx, (-tpy - tcy)/ry, 0)
-    print("end_vector=",end_vector)
     local sweep_angle = angle_between(start_vector, end_vector)
-    print("sweep_angle=",sweep_angle,"in degrees:", sweep_angle*180/math.pi)
 
     if sweep_flag == 0 and sweep_angle > 0 then
         sweep_angle = sweep_angle - 2*math.pi
-        print("B. sweep_angle=",sweep_angle,"in degrees:", sweep_angle*180/math.pi)
     elseif sweep_flag ~= 0 and sweep_angle < 0 then
         sweep_angle = sweep_angle + 2*math.pi
-        print("C. sweep_angle=",sweep_angle,"in degrees:", sweep_angle*180/math.pi)
     end
     sweep_angle = signed_mod(sweep_angle, 2*math.pi)
-    print("FINAL sweep_angle=",sweep_angle,"in degrees:", sweep_angle*180/math.pi)
 
     for i = 1, state.segments do
         local t = i / state.segments
-        print("t=",t)
 
         local a = start_angle + (sweep_angle * t)
-        print("a=",a,"in degrees:", a*180/math.pi)
         local ecx = rx * math.cos(a)
-        print("ecx=",ecx)
         local ecy = ry * math.sin(a)
-        print("ecy=",ecy)
 
-        -- local ec = ecx * state.right + ecy * state.normal
-        -- print("ec=",ec)
-        -- state.current_pos = V.rotate_around_axis(ec, state.forward, angle) + center
         state.current_pos = rotate_around_axis(angle, ecx, ecy, state.right, state.normal) + center
 
-        print("current_pos=",state.current_pos)
         table.insert(state.points, state.current_pos)
     end
     return state
@@ -416,19 +357,12 @@ end
 local cmd_elliptic_arc_curve_abs = function(state, params)
     for i = 1, #params, 7 do
         local rx = math.abs(params[i])
-        print("rx=",rx)
         local ry = math.abs(params[i+1])
-        print("ry=",ry)
         local angle = signed_mod(params[i+2], 360) * math.pi / 180
-        print("angle=",angle)
         local large_arc_flag = params[i+3]
-        print("large_arc_flag=",large_arc_flag)
         local sweep_flag = params[i+4]
-        print("sweep_flag=",sweep_flag)
         local p0 = state.current_pos
-        print("p0=",p0)
         local p1 = state.pos + params[i+5] * state.right + params[i+6] * state.normal
-        print("p1=",p1)
         if p0 ~= p1 then
             state = elliptic_arc_to(state, rx, ry, angle, large_arc_flag, sweep_flag, p0, p1)
         end
@@ -546,7 +480,7 @@ NodeLibrary:addNodes(
                 P.v3("normal", vector(0, 1, 0)),
                 P.v3("right", vector(1, 0, 0)),
                 P.v3("size", vector(1, 1, 1)),
-                P.scalar_int("segments", {default = 36, min = 0, soft_max = 360}),
+                P.scalar_int("segments", {default = 10, min = 0, soft_max = 360}),
             },
             outputs = {P.mesh("out_mesh")},
             returns = "out_mesh"
