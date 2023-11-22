5297dd25761c17fbc1234dca3195d5536bbe3777
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-11-21 19:57:39 -0500

    Add font_library.lua
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

---
+++ blackjack_engine/src/lua_engine/font_library.lua
@@ -0,0 +1,31 @@
+-- Copyright (C) 2023 setzer22 and contributors
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+local FontLibrary = {
+    fonts = {}
+}
+
+function FontLibrary:addFonts(fonts)
+    assert(type(fonts) == "table")
+
+    for k, v in pairs(fonts) do
+        self.fonts[k] = v
+    end
+end
+
+function FontLibrary:listFonts()
+    local fonts = {}
+    for k, _ in pairs(self.fonts) do
+        table.insert(fonts, k)
+    end
+    return fonts
+end
+
+function FontLibrary:getFont(font_name)
+    return self.fonts[font_name]
+end
+
+return FontLibrary
