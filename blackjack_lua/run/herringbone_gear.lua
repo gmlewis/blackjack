cb82c8728525d2fb1820a54f86a40735daedca68
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-03-21 21:47:15 -0400

    Reverse rotation
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- blackjack_lua/run/herringbone_gear.lua
+++ blackjack_lua/run/herringbone_gear.lua
@@ -7,8 +7,8 @@ local POINTS_IN_INVOLUTE_CURVE = 20
 local rotate_point = function(pnt, theta)
     local c = math.cos(theta)
     local s = math.sin(theta)
-    local x = c * pnt.x + s * pnt.z
-    local z = -s * pnt.x + c * pnt.z
+    local x = c * pnt.x - s * pnt.z
+    local z = s * pnt.x + c * pnt.z
     return vector(x, pnt.y, z)
 end
 
@@ -23,6 +23,50 @@ end
 local involute_theta = function(radius, distance)
    local x = distance / radius
    return math.sqrt(x*x - 1)
+end
+
+local gen_pivot_center = function(params)
+   local angle = -math.rad(params.pivot_rotation + params.gear_rotation)
+   return function(face)
+      local new_face = {}
+      for i = 1, #face do
+         local pt = rotate_point(face[i], angle)
+         table.insert(new_face, params.pos + pt)
+      end
+      return new_face
+   end
+end
+
+local gen_pivot_at_radius = function(params, radius)
+   local gear_angle = -math.rad(params.gear_rotation)
+   local pivot_angle = -math.rad(params.pivot_rotation)
+   return function(face)
+      local new_face = {}
+      for i = 1, #face do
+         -- rotate the gear
+         local pt = rotate_point(face[i], gear_angle)
+         -- translate to pivot point
+         pt = pt - vector(-radius,0,0)
+         -- rotate around pivot point
+         pt = rotate_point(pt, pivot_angle)
+         -- translate back to pivot point
+         pt = pt - vector(radius,0,0)
+         table.insert(new_face, params.pos + pt)
+      end
+      return new_face
+   end
+end
+
+local pivot_lookup_func = {
+    Center=gen_pivot_center,
+    RootRadius=function(params) return gen_pivot_at_radius(params, params.root_radius) end,
+    BaseRadius=function(params) return gen_pivot_at_radius(params, params.base_radius) end,
+    PitchRadius=function(params) return gen_pivot_at_radius(params, params.pitch_radius) end,
+    OuterRadius=function(params) return gen_pivot_at_radius(params, params.outer_radius) end,
+}
+
+local gen_rotate_final_face_func = function(params)
+    return pivot_lookup_func[params.pivot](params)
 end
 
 local generate_involute_verts = function(params)
@@ -47,14 +91,14 @@ local generate_involute_verts = function(params)
     local involute = {}
     -- start of tooth
     if (root_radius < base_radius) then
-       local pt = rotate_point(vector(root_radius, 0, 0), center_angle)
+       local pt = rotate_point(vector(root_radius, 0, 0), -center_angle)
        table.insert(involute, pt)
     end
 
     -- lower tooth face
     local angle = start_angle
     for i = 0, POINTS_IN_INVOLUTE_CURVE do
-       local pt = rotate_point(involute_xy(base_radius, angle), center_angle)
+       local pt = rotate_point(involute_xy(base_radius, angle), -center_angle)
        table.insert(involute, pt)
        angle = angle + d_theta
     end
@@ -66,7 +110,7 @@ local generate_involute_verts = function(params)
     local arc_tip_angle = math.abs(math.atan2(arc_tip.z, arc_tip.x))
     for i = 1, params.resolution-1 do
        local phi = -arc_tip_angle + 2*arc_tip_angle*i/params.resolution
-       table.insert(involute, rotate_point(vector(outer_radius, 0, 0), -phi))
+       table.insert(involute, rotate_point(vector(outer_radius, 0, 0), phi))
     end
 
     -- upper tooth face (mirror the lower point)
@@ -80,31 +124,34 @@ local generate_involute_verts = function(params)
     params.pitch_radius = pitch_radius
     params.root_radius = root_radius
     params.outer_radius = outer_radius
+    params.rotate_final_face = gen_rotate_final_face_func(params)
 end
 
 local generate_side_of_tooth = function(faces, last_side_verts, new_side_verts, first_iteration, theta, pt, params)
     for segment = 0, 2 * params.vertical_resolution do
         local y = params.gear_length * segment / (2 * params.vertical_resolution) -- 0..top (max at midpoint)
         local helix_rotation = params.max_helix_rotation - math.abs(params.max_helix_rotation * (-1 + segment/params.vertical_resolution))
-        local vert = vector(0, y, 0) + rotate_point(pt, theta + params.direction*helix_rotation)
+        local vert = vector(0, y, 0) + rotate_point(pt, -theta - params.direction*helix_rotation)
         table.insert(new_side_verts, vert)
 
         if segment > 0 and not first_iteration then
            -- create two faces
-           table.insert(faces, {
-              params.pos + last_side_verts[segment], -- 1-indexed
-              params.pos + new_side_verts[segment],
-              params.pos + new_side_verts[segment + 1],
-           })
-           table.insert(faces, {
-              params.pos + last_side_verts[segment],
-              params.pos + new_side_verts[segment + 1],
-              params.pos + last_side_verts[segment + 1],
-           })
+           table.insert(faces, params.rotate_final_face({
+              last_side_verts[segment], -- 1-indexed
+              new_side_verts[segment],
+              new_side_verts[segment + 1],
+           }))
+           table.insert(faces, params.rotate_final_face({
+              last_side_verts[segment],
+              new_side_verts[segment + 1],
+              last_side_verts[segment + 1],
+           }))
         end
     end
 end
 
+-- note that hole_generator_outline does not rotate the outline into its final place
+-- since the vertices are reused for positioning of the hole.
 local hole_generator_outline = function(faces, top_tooth, bottom_tooth, last_side_verts, params)
    local tpt1 = top_tooth[1]
    local tpt2 = top_tooth[#top_tooth]
@@ -113,10 +160,10 @@ local hole_generator_outline = function(faces, top_tooth, bottom_tooth, last_sid
    local bpt2 = bottom_tooth[1]
    local bot_face = { bpt2, bpt1 }
    for j = 0, params.resolution-1 do
-      local tnext2 = params.pos + rotate_point(tpt2-params.pos, params.gap_delta)
+      local tnext2 = rotate_point(tpt2, -params.gap_delta)
       table.insert(top_face, tnext2)
       tpt2 = tnext2
-      local bnext2 = params.pos + rotate_point(bpt2-params.pos, params.gap_delta)
+      local bnext2 = rotate_point(bpt2, -params.gap_delta)
       table.insert(bot_face, 1, bnext2)  -- reversed to that normal is correct
       bpt2 = bnext2
    end
@@ -125,25 +172,22 @@ end
 
 local hole_generator_none = function(faces, top_tooth, bottom_tooth, last_side_verts, params)
    local top_face, bot_face = hole_generator_outline(faces, top_tooth, bottom_tooth, last_side_verts, params)
-   table.insert(top_face, params.pos + params.top)
-   table.insert(bot_face, params.pos)
+   table.insert(top_face, params.top)
+   table.insert(bot_face, vector(0,0,0))
 
-   table.insert(faces, top_face)
-   table.insert(faces, bot_face)
+   table.insert(faces, params.rotate_final_face(top_face))
+   table.insert(faces, params.rotate_final_face(bot_face))
 end
 
 local hole_generator_with_hole = function(faces, top_tooth, bottom_tooth, last_side_verts, params, hole_outline_func)
    local top_face, bot_face = hole_generator_outline(faces, top_tooth, bottom_tooth, last_side_verts, params)
 
-   local pos = params.pos
    local top = params.top
    local tpt1 = top_tooth[1]
-   local tmp1 = tpt1-pos
-   local start_theta = math.atan2(tmp1.z, tmp1.x)
+   local start_theta = math.atan2(tpt1.z, tpt1.x)
    local tpt2 = top_tooth[#top_tooth]
-   local tmp2 = tpt2-pos
    local gap_arc_length = params.resolution * params.gap_delta
-   local end_theta = math.atan2(tmp2.z, tmp2.x) - gap_arc_length
+   local end_theta = math.atan2(tpt2.z, tpt2.x) - gap_arc_length
 
    while end_theta > start_theta do
       end_theta = end_theta - 2*math.pi
@@ -152,9 +196,9 @@ local hole_generator_with_hole = function(faces, top_tooth, bottom_tooth, last_s
    for j = 0, params.resolution do
       local t = j / params.resolution -- t = 0..1
       local theta1 = start_theta + (1 - t) * (end_theta - start_theta)
-      table.insert(top_face, pos + top + rotate_point(hole_outline_func(-theta1), -theta1))
+      table.insert(top_face, top + rotate_point(hole_outline_func(-theta1), theta1))
       local theta2 = start_theta + t * (end_theta - start_theta)
-      table.insert(bot_face, pos + rotate_point(hole_outline_func(-theta2), -theta2))
+      table.insert(bot_face, rotate_point(hole_outline_func(-theta2), theta2))
    end
 
    for j = 0, params.resolution-1 do
@@ -163,11 +207,11 @@ local hole_generator_with_hole = function(faces, top_tooth, bottom_tooth, last_s
       local v2 = top_face[#top_face-params.resolution+j]
       local v3 = bot_face[#bot_face-j]
       local v4 = bot_face[#bot_face-j-1]
-      table.insert(faces, {v1, v2, v3, v4})
+      table.insert(faces, params.rotate_final_face({v1, v2, v3, v4}))
    end
 
-   table.insert(faces, top_face)
-   table.insert(faces, bot_face)
+   table.insert(faces, params.rotate_final_face(top_face))
+   table.insert(faces, params.rotate_final_face(bot_face))
 end
 
 local hole_generator_circular = function(faces, top_tooth, bottom_tooth, last_side_verts, params)
@@ -239,15 +283,17 @@ local generate_teeth = function(params)
     local faces = {}
     for i = 0, params.num_teeth - 1 do
         local theta = i * 2 * math.pi / params.num_teeth
+        -- note that top_tooth and bottom_tooth are not rotated into final position
+        -- since they are reused to create hole positions.
         local top_tooth = {}
         local bottom_tooth = {}
 
         -- create the tooth
         for j = 1, #involute do
             local rev_pt = involute[#involute - j + 1]
-            table.insert(top_tooth, params.pos + params.top + rotate_point(rev_pt, theta))
+            table.insert(top_tooth, params.top + rotate_point(rev_pt, -theta))
             local pt = involute[j]
-            table.insert(bottom_tooth, params.pos + rotate_point(pt, theta))
+            table.insert(bottom_tooth, rotate_point(pt, -theta))
             -- generate one side of the tooth
             local new_side_verts = {}
             generate_side_of_tooth(faces, last_side_verts, new_side_verts, j==1, theta, rev_pt, params)
@@ -255,8 +301,8 @@ local generate_teeth = function(params)
         end
 
         if params.hole_type ~= "Hollow" then
-           table.insert(faces, top_tooth)
-           table.insert(faces, bottom_tooth)
+           table.insert(faces, params.rotate_final_face(top_tooth))
+           table.insert(faces, params.rotate_final_face(bottom_tooth))
         end
 
         -- create arc at root_radius across inner tooth edge
@@ -264,10 +310,10 @@ local generate_teeth = function(params)
            local last_pt1 = last_side_verts[segment]
            local last_pt2 = last_side_verts[segment + 1]
            for j = 1, params.resolution do
-              local pt1 = rotate_point(last_pt1, params.gap_delta)
-              local pt2 = rotate_point(last_pt2, params.gap_delta)
-              table.insert(faces, { params.pos+last_pt1, params.pos+pt1, params.pos+pt2 })
-              table.insert(faces, { params.pos+last_pt1, params.pos+pt2, params.pos+last_pt2 })
+              local pt1 = rotate_point(last_pt1, -params.gap_delta)
+              local pt2 = rotate_point(last_pt2, -params.gap_delta)
+              table.insert(faces, params.rotate_final_face({ last_pt1, pt1, pt2 }))
+              table.insert(faces, params.rotate_final_face({ last_pt1, pt2, last_pt2 }))
               last_pt1 = pt1
               last_pt2 = pt2
            end
@@ -318,6 +364,7 @@ NodeLibrary:addNodes(
                 }
             end,
             inputs = {
+                -- pos sets the pivot point in 3D space.
                 P.v3("pos", vector(0, 0, 0)),
                 -- Use the "PitchRadius" for meshing with other gears.
                 -- "Center" is the center of the gear.
